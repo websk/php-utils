9300ffe81a400cd4db4b7a97f8213a155653ea0c
skulkov <sergey.kulkov@gmail.com>
2018-10-23 16:46:13 +0300

    Sanitize

---
+++ src/WebSK/Utils/Sanitize.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace WebSK\Utils;
+
+/**
+ * Class Sanitize
+ * @package WebSK\Utils
+ */
+class Sanitize
+{
+    /**
+     * @param string $value
+     * @return string
+     */
+    public static function sanitizeTagContent(string $value)
+    {
+        $value = htmlspecialchars($value);
+
+        return $value;
+    }
+
+    /**
+     * @param string $url
+     * @return string
+     */
+    public static function sanitizeUrl(string $url)
+    {
+        return filter_var($url, FILTER_SANITIZE_URL);
+    }
+
+    /**
+     * @param string $value
+     * @return string
+     */
+    public static function sanitizeAttrValue(string $value)
+    {
+        return htmlspecialchars($value, ENT_QUOTES | ENT_HTML5);
+    }
+
+    /**
+     * @param string $column_name
+     * @return null|string|string[]
+     */
+    public static function sanitizeSqlColumnName(string $column_name){
+        $column_name = preg_replace("/[^a-zA-Z0-9_]+/", "", $column_name);
+        return $column_name;
+    }
+}
