69834b463d3e5acc9608a9cd7299c7079531669c
skulkov <sergey.kulkov@gmail.com>
2019-04-25 17:55:47 +0300

    cookie message

--- src/WebSK/Utils/Messages.php
+++ src/WebSK/Utils/Messages.php
@@ -8,13 +8,25 @@ namespace WebSK\Utils;
  */
 class Messages
 {
+    const MESSAGES_COOKIE_NAME = 'messages';
+
     /**
      * @param string $key
      * @param string $value
      */
     protected static function setMessageValue(string $key, string $value)
     {
-        $_SESSION['messages'][$key] = $value;
+        $cookie_value_arr = self::getMessagesArr();
+
+        if (!is_array($cookie_value_arr[$key])) {
+            $cookie_value_arr[$key] = [];
+        }
+
+        array_push($cookie_value_arr[$key], $value);
+
+        $value_serialize = serialize($cookie_value_arr);
+
+        setcookie(self::MESSAGES_COOKIE_NAME , $value_serialize, 0, '/');
     }
 
     /**
@@ -46,20 +58,42 @@ class Messages
      */
     public static function renderMessages()
     {
-        if (!isset($_SESSION)) {
-            return '';
-        }
-
-        if (!array_key_exists('messages', $_SESSION)) {
-            return '';
-        }
+        $cookie_value_arr = self::getMessagesArr();
 
         $messages = '';
-        foreach ($_SESSION['messages'] as $key => $message) {
-            $messages .= '<p class="alert alert-' . $key . ' flash-' . $key . '">' . $message . "</p>";
-            unset($_SESSION['messages'][$key]);
+        foreach ($cookie_value_arr as $key => $messages_arr) {
+            if (is_array($messages_arr)) {
+                continue;
+            }
+
+            $messages .= '<p class="alert alert-' . $key . ' flash-' . $key . '">';
+            foreach ($messages_arr as $message) {
+                $messages .= $message . '<br>';
+            }
+            $messages .= "</p>";
+
+            setcookie(self::MESSAGES_COOKIE_NAME , '', time() - 3600, '/');
         }
 
         return $messages;
+    }
+
+    /**
+     * @return array
+     */
+    protected static function getMessagesArr()
+    {
+        if (!isset($_COOKIE)) {
+            return [];
+        }
+
+        if (!array_key_exists(self::MESSAGES_COOKIE_NAME, $_COOKIE)) {
+            return [];
+        }
+
+        $value_serialize = $_COOKIE[self::MESSAGES_COOKIE_NAME];
+        $value_arr = unserialize($value_serialize);
+
+        return $value_arr;
     }
 }
